7b203fa4c68fcf8a673bc0cd8a6a3dff733d4436
adlange <lange@iig.uni-freiburg.de>
2014-01-18 15:15:54 +0000

    - Adopted the recent changes in the data usage extension - Parse data values by their type - Bugfix with originator candidates

--- src/de/uni/freiburg/iig/telematik/jawl/parser/LogParser.java
+++ src/de/uni/freiburg/iig/telematik/jawl/parser/LogParser.java
@@ -40,10 +40,6 @@ import de.uni.freiburg.iig.telematik.jawl.log.LogTrace;
 public class LogParser {
 
 	private XUniversalParser parser = new XUniversalParser();
-	
-	public LogParser(){
-		parser = new XUniversalParser();
-	}
 
 	/**
 	 * Checks whether the given file can be parsed by the file extension.
@@ -95,23 +91,23 @@ public class LogParser {
 			Collection<LogTrace> logTraces = new ArrayList<LogTrace>();
 			for (XTrace trace : log) {
 				Integer traceID = null;
-				
+
 				// Extract trace ID
 				for (Map.Entry<String, XAttribute> attribute : trace.getAttributes().entrySet()) {
 					String key = attribute.getKey();
 					String value = attribute.getValue().toString();
-					if(key.equals("concept:name")){
+					if (key.equals("concept:name")) {
 						try {
 							Validate.notNegativeInteger(value);
 							traceID = Integer.parseInt(value);
-						} catch(ParameterException e){
+						} catch (ParameterException e) {
 							throw new ParserException("Cannot extract case-id.");
 						}
 					}
 				}
-				if(traceID == null)
+				if (traceID == null)
 					throw new ParserException("Cannot extract case-id");
-				
+
 				// Build new log trace
 				LogTrace logTrace = new LogTrace(traceID);
 				for (XEvent event : trace) {
@@ -125,8 +121,8 @@ public class LogParser {
 
 		return logTracesCollection;
 	}
-	
-	private LogEntry buildLogEntry(XEvent xesEvent) throws ParserException{
+
+	private LogEntry buildLogEntry(XEvent xesEvent) throws ParserException, ParameterException {
 		LogEntry logEntry = new LogEntry();
 		for (Map.Entry<String, XAttribute> attribute : xesEvent.getAttributes().entrySet()) {
 			String key = attribute.getKey();
@@ -138,7 +134,7 @@ public class LogParser {
 				addEventType(logEntry, attribute.getValue().toString());
 			} else if (key.equals("time:timestamp")) {
 				addTimestamp(logEntry, attribute.getValue().toString());
-			} else if (key.equals("dataUsage:dataUsage")) {
+			} else if (key.equals("dataUsage:data")) {
 				addDataUsage(logEntry, attribute);
 			} else {
 				// If the key is unknown, a meta attribute with the key/value pair is added
@@ -147,9 +143,9 @@ public class LogParser {
 		}
 		return logEntry;
 	}
-	
-	private void addName(LogEntry entry, String value) throws ParserException{
-		if(value == null || value.isEmpty())
+
+	private void addName(LogEntry entry, String value) throws ParserException {
+		if (value == null || value.isEmpty())
 			throw new ParserException("No value for concept:name");
 		try {
 			entry.setActivity(value);
@@ -157,19 +153,22 @@ public class LogParser {
 			throw new ParserException("Cannot set activity of log entry: " + e.getMessage());
 		}
 	}
-	
-	private void addOriginator(LogEntry entry, String value) throws ParserException{
-		if(value == null || value.isEmpty())
+
+	private void addOriginator(LogEntry entry, String value) throws ParserException {
+		if (value == null || value.isEmpty())
 			throw new ParserException("No value for org:resource");
 		try {
+			if (!entry.getOriginatorCandidates().contains(value))
+				entry.addOriginatorCandidate(value);
 			entry.setOriginator(value);
 		} catch (Exception e) {
+			e.printStackTrace();
 			throw new ParserException("Cannot set originator of log entry: " + e.getMessage());
 		}
 	}
-	
-	private void addEventType(LogEntry entry, String value) throws ParserException{
-		if(value == null || value.isEmpty())
+
+	private void addEventType(LogEntry entry, String value) throws ParserException {
+		if (value == null || value.isEmpty())
 			throw new ParserException("No value for lifecycle:transition");
 		EventType eventType = EventType.parse(value);
 		if (eventType == null)
@@ -180,9 +179,9 @@ public class LogParser {
 			throw new ParserException("Cannot set event type of log entry: " + e.getMessage());
 		}
 	}
-	
-	private void addTimestamp(LogEntry entry, String value) throws ParserException{
-		if(value == null || value.isEmpty())
+
+	private void addTimestamp(LogEntry entry, String value) throws ParserException {
+		if (value == null || value.isEmpty())
 			throw new ParserException("No value for time:timestamp");
 		Date date = null;
 		String sanitizedDateString = value.replaceAll(":(\\d\\d)$", "$1");
@@ -198,25 +197,21 @@ public class LogParser {
 				}
 			}
 		}
-		if(date == null)
+		if (date == null)
 			throw new ParserException("Cannot read timestamp.");
-		
+
 		try {
 			entry.setTimestamp(date);
 		} catch (Exception e) {
 			throw new ParserException("Cannot set log entry timestamp: " + e.getMessage());
 		}
 	}
-	
-	private void addDataUsage(LogEntry entry, Map.Entry<String, XAttribute> attribute) throws ParserException{
+
+	private void addDataUsage(LogEntry entry, Map.Entry<String, XAttribute> attribute) throws ParameterException {
 		// Get sub-attributes
 		for (Map.Entry<String, XAttribute> xAttribute : attribute.getValue().getAttributes().entrySet()) {
 			String dataAttributeKey = xAttribute.getKey();
-			String dataAttributeValue = xAttribute.getValue().toString();
-			// FIXME all values as string?
-			// -> If xAttribute allows to identify the value type, then try to cast to appropriate class
-			//    Maybe in the extension of the attribute value?
-			// -> Data usage modes have to be cast into DataUsage
+			Object dataAttributeValue = parseAttributeValue(xAttribute.getValue());
 			DataAttribute dataAttribute = new DataAttribute(dataAttributeKey, dataAttributeValue);
 			String dataAttributeDataUsageString = null;
 			for (Map.Entry<String, XAttribute> dataUsage : xAttribute.getValue().getAttributes().entrySet()) {
@@ -238,31 +233,46 @@ public class LogParser {
 			}
 		}
 	}
-	
-	private void addMetaInformation(LogEntry entry, Map.Entry<String, XAttribute> attribute) throws ParserException{
+
+	private void addMetaInformation(LogEntry entry, Map.Entry<String, XAttribute> attribute) throws ParserException {
 		entry.addMetaAttribute(new DataAttribute(attribute.getKey(), attribute.getValue()));
 	}
 
 	/**
-	 * Takes a String containing {@link DataUsage} identifier separated by commas, removes every leading and training whitespace, and parses them into a {@link List}. TODO move to TOVAL into enum DataUsage?
+	 * Tries to parse the value of a {@link XAttribute} to a numeric, boolean, or string value.
 	 */
-	public static List<DataUsage> parseDataUsageString(String dataUsageString) {
+	private Object parseAttributeValue(XAttribute xAttribute) {
+		String attributeString = xAttribute.toString();
+
+		// TODO better solution?
+
+		// All numeric values as double
+		try {
+			double a = Double.parseDouble(attributeString);
+			return a;
+		} catch (NumberFormatException e) {
+			// Ignore
+		}
+		// Boolean
+		if (attributeString.trim().toLowerCase().equals("true"))
+			return true;
+		if (attributeString.trim().toLowerCase().equals("false"))
+			return false;
+		// String
+		return attributeString;
+	}
+
+	/**
+	 * Takes a String containing {@link DataUsage} identifier separated by commas, removes every leading and training whitespace, and parses them into a {@link List}. <br>
+	 * TODO move to TOVAL into enum {@link DataUsage}?
+	 */
+	private static List<DataUsage> parseDataUsageString(String dataUsageString) throws ParameterException {
 		List<String> dataUsageStrings = Arrays.asList(dataUsageString.split("\\s*,\\s*"));
 		List<DataUsage> dataUsageList = new Vector<DataUsage>(dataUsageStrings.size());
 		for (String d : dataUsageStrings) {
-			try {
-				DataUsage dataUsage = DataUsage.parse(d);
-				if (!dataUsageList.contains(dataUsage))
-					dataUsageList.add(dataUsage);
-			} catch (ParameterException e) {
-				// FIXME ignore exception if file contains invalid DataUsage strings?
-				// -> Throw exception!
-				// -> In this case the input file contains invalid content.
-				// -> Include line number of input file in exception if possible.
-				// -> Use Toval Parser Exception (inherit if necessary to include additional fields)
-				
-				// e.printStackTrace();
-			}
+			DataUsage dataUsage = DataUsage.parse(d);
+			if (!dataUsageList.contains(dataUsage))
+				dataUsageList.add(dataUsage);
 		}
 		return dataUsageList;
 	}
