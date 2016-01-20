890fc5fbfde7157d50ba78a0120eee8b1a8271e7
adlange <lange@iig.uni-freiburg.de>
2016-01-20 14:18:37 +0100

    added LogView as a filterable adapter for Log

--- src/de/uni/freiburg/iig/telematik/sewol/log/LogSummary.java
+++ src/de/uni/freiburg/iig/telematik/sewol/log/LogSummary.java
@@ -10,46 +10,54 @@ import de.invation.code.toval.validate.ParameterException;
 import de.invation.code.toval.validate.Validate;
 
 public class LogSummary<E extends LogEntry> {
-	
-	private final Set<String> activities = new HashSet<>();
-	private final Set<String> originators = new HashSet<>();
-	private final Set<String> roles = new HashSet<>();
-	private final Observation traceLength = new Observation();
-	
-	public LogSummary() {}
 
-	public LogSummary(List<LogTrace<E>> traces) throws ParameterException {
-		addTraces(traces);
-	}
-	
-	public final void addTraces(List<LogTrace<E>> traces) throws ParameterException {
-		Validate.notNull(traces);
-		for(LogTrace<E> trace: traces){
-			addTrace(trace);
-		}
-	}
-	
-	public void addTrace(LogTrace<E> trace) throws ParameterException{
-		Validate.notNull(trace);
-		activities.addAll(trace.getDistinctActivities());
-		originators.addAll(trace.getDistinctOriginators());
-		roles.addAll(trace.getDistinctRoles());
-		traceLength.addValue(trace.size());
-	}
+        private final Set<String> activities = new HashSet<>();
+        private final Set<String> originators = new HashSet<>();
+        private final Set<String> roles = new HashSet<>();
+        private final Observation traceLength = new Observation();
 
-	public Set<String> getActivities() {
-		return Collections.unmodifiableSet(activities);
-	}
+        public LogSummary() {
+        }
 
-	public Set<String> getOriginators() {
-		return Collections.unmodifiableSet(originators);
-	}
+        public LogSummary(List<LogTrace<E>> traces) throws ParameterException {
+                addTraces(traces);
+        }
 
-	public Set<String> getRoles() {
-		return Collections.unmodifiableSet(roles);
-	}
-	
-	public double getAverageTraceLength(){
-		return traceLength.getAverage();
+        public final void addTraces(List<LogTrace<E>> traces) throws ParameterException {
+                Validate.notNull(traces);
+                for (LogTrace<E> trace : traces) {
+                        addTrace(trace);
+                }
+        }
+
+        public void addTrace(LogTrace<E> trace) throws ParameterException {
+                Validate.notNull(trace);
+                activities.addAll(trace.getDistinctActivities());
+                originators.addAll(trace.getDistinctOriginators());
+                roles.addAll(trace.getDistinctRoles());
+                traceLength.addValue(trace.size());
+        }
+
+        public Set<String> getActivities() {
+                return Collections.unmodifiableSet(activities);
+        }
+
+        public Set<String> getOriginators() {
+                return Collections.unmodifiableSet(originators);
+        }
+
+        public Set<String> getRoles() {
+                return Collections.unmodifiableSet(roles);
+        }
+
+        public double getAverageTraceLength() {
+                return traceLength.getAverage();
+        }
+
+        public void clear() {
+                activities.clear();
+                originators.clear();
+                roles.clear();
+                traceLength.reset();
         }
 }
